d751e845efc530b6930b371e412bb8c3f186fa8c
hohjukgi <20205073@g.aut.ac.jp>
2022-11-18 15:41:47 +0900

    サイズ調整

--- SoftwareDevelopmentProjects/Pikokon.Designer.cs
+++ SoftwareDevelopmentProjects/Pikokon.Designer.cs
@@ -78,11 +78,11 @@ namespace SoftwareDevelopmentProjects
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(276, 31);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 24);
+            this.label2.Size = new System.Drawing.Size(91, 37);
             this.label2.TabIndex = 8;
             this.label2.Text = "時間";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -139,7 +139,7 @@ namespace SoftwareDevelopmentProjects
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(610, 451);
+            this.ClientSize = new System.Drawing.Size(794, 451);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.buttonDelete);
             this.Controls.Add(this.listView1);
