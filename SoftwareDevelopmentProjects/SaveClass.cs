9e64e46c70e677edb50cc7ea18ef549af8a51537
hohjukgi <20205073@g.aut.ac.jp>
2022-11-25 13:58:48 +0900

    テンプレ追加

---
+++ SoftwareDevelopmentProjects/SaveClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDevelopmentProjects
+{
+    public static class SaveClass
+    {
+        //変換した適す値
+        private static string exportText;
+
+        /// <summary>
+        /// 保存してあるデータをexportTextにまとめる
+        /// </summary>
+        public static void ConvertToSaveData()
+        {
+
+        }
+
+        /// <summary>
+        /// exportTextをCSV形式に出力
+        /// </summary>
+        public static void ExportCsv()
+        {
+
+        }
+    }
+}
