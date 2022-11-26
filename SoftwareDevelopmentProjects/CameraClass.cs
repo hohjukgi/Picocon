37cf0d7d8d673f0be9abe4cad1863099ce240f23
hohjukgi <70085361+RFTtama@users.noreply.github.com>
2022-11-26 19:03:00 +0900

    python for windows を削除 写真を撮るメソッドを追加

---
+++ SoftwareDevelopmentProjects/CameraClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace SoftwareDevelopmentProjects
+{
+    internal class CameraClass
+    {
+        private Mat _flame;
+
+        public Bitmap TakePhoto()
+        {
+            using (var capture = new VideoCapture())
+            {
+                //カメラの起動　
+                capture.Open(0);
+
+                if (!capture.IsOpened())
+                {
+                    throw new Exception("capture initialization failed");
+                }
+
+                //画像取得用のMatを作成
+                _flame = new Mat();
+
+                while (true)
+                {
+                    try
+                    {
+                        capture.Read(_flame);
+                        if (_flame.Empty())
+                        {
+                            break;
+                        }
+
+                        if (_flame.Size().Width > 0)
+                        {
+                            //MatをBitMapに変換
+                            return BitmapConverter.ToBitmap(_flame);
+                        }
+
+                    }
+
+                    catch (Exception)
+                    {
+                        break;
+                    }
+                }
+                return null;
+            }
+        }
+    }
+}
