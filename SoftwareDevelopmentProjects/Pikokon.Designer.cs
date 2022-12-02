7978b46bb9397afdc3e8e3f369b552fd215feea3
hohjukgi <20205073@g.aut.ac.jp>
2022-12-02 15:41:17 +0900

    コンテキストメニューストリップを削除

--- SoftwareDevelopmentProjects/Pikokon.Designer.cs
+++ SoftwareDevelopmentProjects/Pikokon.Designer.cs
@@ -58,7 +58,6 @@
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.labelName = new System.Windows.Forms.PictureBox();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -380,11 +379,6 @@
             this.labelName.TabIndex = 12;
             this.labelName.TabStop = false;
             this.labelName.Click += new System.EventHandler(this.labelName_Click);
-            // 
-            // contextMenuStrip1
-            // 
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // Pikokon
             // 
