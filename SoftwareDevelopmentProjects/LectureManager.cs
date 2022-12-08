2513d11072b79b85e894bb82266a65c9841294aa
hohjukgi <20205073@g.aut.ac.jp>
2022-12-08 12:13:02 +0900

    講義を扱うクラスを追加

---
+++ SoftwareDevelopmentProjects/LectureManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDevelopmentProjects
+{
+    class LectureManager
+    {
+        private List<string> _lectureList;
+        public List<string> lectureList
+        {
+            get
+            {
+                return _lectureList;
+            }
+        }
+        public string fileName;
+
+        public LectureManager()
+        {
+            _lectureList = new List<string>();
+            this.fileName = string.Empty;
+        }
+
+        public LectureManager(string fileName)
+        {
+            _lectureList = new List<string>();
+            this.fileName = fileName;
+        }
+
+        public void ReadLectureDatas()
+        {
+            if(fileName == "")
+            {
+                LogManager.LogOutput("講義データファイルが指定されていません");
+                return;
+            }
+
+        }
+    }
+}
