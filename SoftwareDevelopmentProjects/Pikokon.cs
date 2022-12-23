1ccd9468829d0176d97f309537ba08ecac9ecdee
山下怜也 <20205014@g.aut.ac.jp>
2022-12-23 16:29:56 +0900

    ランダム音声

--- SoftwareDevelopmentProjects/Pikokon.cs
+++ SoftwareDevelopmentProjects/Pikokon.cs
@@ -282,13 +282,81 @@ namespace SoftwareDevelopmentProjects
                         //オーディオ再生処理
                         Task task = Task.Run(() =>
                         {
-                        //オーディオリソースを取り出す
-                        System.IO.Stream strm = Properties.Resources.Motion_Pop02_1__online_audio_converter_com_;
-                        //同期再生する
-                        System.Media.SoundPlayer player = new System.Media.SoundPlayer(strm);
-                            player.PlaySync();
-                        //後始末
-                        player.Dispose();
+                        Random r = new Random();
+                        int randomValue = r.Next(100);
+                        if (randomValue == 0)
+                            {
+                                //オーディオリソースを取り出す
+                                System.IO.Stream strm = Properties.Resources.lucky_sound;
+                                //同期再生する
+                                System.Media.SoundPlayer player = new System.Media.SoundPlayer(strm);
+                                player.PlaySync();
+                                //後始末
+                                player.Dispose();
+                            } else if (randomValue == 1)
+                            {
+                                //オーディオリソースを取り出す
+                                System.IO.Stream strm = Properties.Resources.lucky_sound2;
+                                //同期再生する
+                                System.Media.SoundPlayer player = new System.Media.SoundPlayer(strm);
+                                player.PlaySync();
+                                //後始末
+                                player.Dispose();
+                            } else if (randomValue == 2)
+                            {
+                                //オーディオリソースを取り出す
+                                System.IO.Stream strm = Properties.Resources.lucky_sound3;
+                                //同期再生する
+                                System.Media.SoundPlayer player = new System.Media.SoundPlayer(strm);
+                                player.PlaySync();
+                                //後始末
+                                player.Dispose();
+                            } else if (randomValue == 3)
+                            {
+                                //オーディオリソースを取り出す
+                                System.IO.Stream strm = Properties.Resources.lucky_sound4;
+                                //同期再生する
+                                System.Media.SoundPlayer player = new System.Media.SoundPlayer(strm);
+                                player.PlaySync();
+                                //後始末
+                                player.Dispose();
+                            } else if (randomValue == 4)
+                            {
+                                //オーディオリソースを取り出す
+                                System.IO.Stream strm = Properties.Resources.lucky_sound5;
+                                //同期再生する
+                                System.Media.SoundPlayer player = new System.Media.SoundPlayer(strm);
+                                player.PlaySync();
+                                //後始末
+                                player.Dispose();
+                            } else if (randomValue == 5)
+                            {
+                                //オーディオリソースを取り出す
+                                System.IO.Stream strm = Properties.Resources.lucky_sound6;
+                                //同期再生する
+                                System.Media.SoundPlayer player = new System.Media.SoundPlayer(strm);
+                                player.PlaySync();
+                                //後始末
+                                player.Dispose();
+                            } else if (randomValue == 6)
+                            {
+                                //オーディオリソースを取り出す
+                                System.IO.Stream strm = Properties.Resources.lucky_sound7;
+                                //同期再生する
+                                System.Media.SoundPlayer player = new System.Media.SoundPlayer(strm);
+                                player.PlaySync();
+                                //後始末
+                                player.Dispose();
+                            } else
+                            {
+                                //オーディオリソースを取り出す
+                                System.IO.Stream strm = Properties.Resources.Motion_Pop02_1__online_audio_converter_com_;
+                                //同期再生する
+                                System.Media.SoundPlayer player = new System.Media.SoundPlayer(strm);
+                                player.PlaySync();
+                                //後始末
+                                player.Dispose();
+                            }
                         });
 
                         //時間を取得
